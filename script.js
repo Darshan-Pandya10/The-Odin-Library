b37836fb3f7b4fad8de3de3e8e4a617e883d2f07
Darshan Pandya <80563657+Darshan-Pandya10@users.noreply.github.com>
2022-09-14 04:00:11 +0530

    [ADD] fetching data from user input and showing inside book card.

--- script.js
+++ script.js
@@ -1,46 +1,106 @@
 const body = document.body;
 const libraryDiv = document.querySelector('.library');
 const addNewBook = document.querySelector('.addNewBook');
-const formDiv = document.querySelector('.formdiv');
 const form = document.querySelector('form');
 const cross = document.querySelector('.cross')
-let library = []
 const header = document.querySelector('header');
+
+
 // detail fatch
-const bookName = document.querySelector('bookname');
-const authorName = document.querySelector('authorname');
+
+
+let library = []
+const bookName = document.querySelector('.bookname');
+const authorName = document.querySelector('.authorname');
 const description = document.querySelector('.Description');
 const pages = document.querySelector('.pages');
 const bookEdition = document.querySelector('.bookedition');
-const submit = document.querySelector('.submit');
+const add = document.querySelector('.add');
 
-
+function Book(bookName,authorName,description,pages,bookEdition) {
+    this.name = bookName;
+    this.author = authorName;
+    this.description = description;
+    this.pages = pages;
+    this.edition = bookEdition;
+  }
 
 // form pop-up start
 addNewBook.addEventListener('click' , (e) => {
     e.preventDefault();
     form.classList.add('formshow');
     console.log('button clicked');
+    addNewBook.disabled = true;
 })
 
 cross.addEventListener('click',() => {
     form.classList.remove('formshow');
+    addNewBook.disabled = false;
+
 })
 
 // form pop-up end
 
-// function Book(name,author,pages,) {
-//     this.name = ;
-//     this.lastName = last;
-//     this.age = age;
-//     this.eyeColor = eye;
-//   }
+// creating new object & adding to the library
 
-// creating new object
-const newBook = document.createElement('div');
-newBook.classList.add('newBook');
-library.push(newBook)
-libraryDiv.appendChild(newBook)
-console.log(library)
+add.addEventListener('click',(e) => {
+
+    e.preventDefault()
+    let book = new Book(bookName.value,authorName.value,description.value,pages.value,bookEdition.value)
+    library.push(book);
+    console.log(book);
+    console.log(library);
+    form.classList.remove('formshow');
+    addNewBook.disabled = false;
+
+        
+    let bookCard = document.createElement('div');
+    bookCard.classList.add('bookCard');
+    libraryDiv.appendChild(bookCard);
 
 
+    let cardHeader = document.createElement('div');
+    cardHeader.classList.add('cardHeader');
+    bookCard.appendChild(cardHeader);
+
+    let bookTitle = document.createElement('p');
+    bookTitle.classList.add('bookTitle');
+    cardHeader.appendChild(bookTitle);
+    bookTitle.innerText = `Name : ${bookName.value}`
+
+    let author = document.createElement('p');
+    author.classList.add('author');
+    cardHeader.appendChild(author); 
+    author.innerText = `Author : ${authorName.value}`
+
+
+    // other details regarding book
+
+    let otherDetails = document.createElement('div');
+    otherDetails.classList.add('otherDetails');
+    bookCard.appendChild(otherDetails);
+
+    let descriptionBox = document.createElement('div');
+    descriptionBox.classList.add('descriptionBox');
+    otherDetails.appendChild(descriptionBox);
+
+    let descriptionText = document.createElement('p');
+    descriptionText.classList.add('descriptionText');
+    descriptionBox.appendChild(descriptionText);
+    descriptionText.innerText = `Description : ${description.value}`
+ 
+
+    let totalPages = document.createElement('p');
+    totalPages.classList.add('totalPages');
+    otherDetails.appendChild(totalPages); 
+    totalPages.innerText = `Pages : ${pages.value}`
+
+
+    let editionOfBook = document.createElement('p');
+    editionOfBook.classList.add('editionOfBook');
+    otherDetails.appendChild(editionOfBook); 
+    editionOfBook.innerText = `Edition : ${bookEdition.value}`
+
+})
+
+// to fetch data from library array and display into book card.
