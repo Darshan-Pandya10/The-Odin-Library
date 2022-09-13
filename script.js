d5aad945e5071050629a5b8d17727e6806bf6bb6
Darshan Pandya <80563657+Darshan-Pandya10@users.noreply.github.com>
2022-09-14 04:50:58 +0530

    [ADD] Update Delete functionality with DOM.

--- script.js
+++ script.js
@@ -102,6 +102,7 @@ function CardBook(){
         e.preventDefault();
         library.pop(this.book);
         console.log(library)
+        bookCard.remove()
     })
 
     // other details regarding book
